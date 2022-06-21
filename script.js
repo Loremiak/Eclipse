1c27dbb01a5a541528c0a334efdef61c4869d2a8
Damian Jamróz <mrlorem@gmail.com>
2022-06-21 19:13:43 +0200

    finish

--- script.js
+++ script.js
@@ -1,88 +1,77 @@
 function getAPI() {
 	fetch(
-		`https://api.unsplash.com/photos/random?count=2&client_id=${env.UNSPLASH_API_KEY}`
-	) //count=2&
+		"https://api.unsplash.com/photos/random?count=4&client_id="
+	)
+		// `https://api.unsplash.com/photos/random?count=2&client_id=${env.UNSPLASH_API_KEY}`
 		.then((response) => {
 			return response.json();
 		})
 		.then((data) => {
-			// console.log(data);
-			// console.log(data[0].urls.full); // losowe img
-			// console.log(data[0].user.name); // nazwa autora
-			// console.log(data[0].created_at); // data i godzina
-			// console.log(data[0].user.portfolio_url); // portfolio link
-			// console.log(data[0].current_user_collections.title); // title
+			const mainContainer = document.querySelector(".main-container");
 
-			const authorName = document.querySelector(".author-name");
-			const createdDate = document.querySelector(".created-date");
-			const imgTitle = document.querySelector(".img-title");
-			const imgClass = document.querySelectorAll(".img-class");
-			console.log(imgClass);
-			data.forEach((photos, idx) => {
-				console.log(photos);
+			data.forEach((photos) => {
+				const boxDOM = document.createElement("div");
+				boxDOM.setAttribute("class", "box");
+				mainContainer.appendChild(boxDOM);
 
-				let userName = photos.user.name;
-				authorName.innerText = userName;
-				console.log(userName);
-
-				let portfolioUrl = photos.user.portfolio_url;
-
-				if (portfolioUrl !== null) {
-					authorName.setAttribute("href", portfolioUrl);
-					console.log(portfolioUrl);
-				}
-
-				let date = new Date(photos.created_at).toLocaleDateString("en-NZ");
-				createdDate.innerText = date;
-				console.log(date);
-
+				const boxTitleDOM = document.createElement("p");
+				boxTitleDOM.setAttribute("class", "box-title");
 				let imgTitleAPI = photos.current_user_collections.title;
 				if (imgTitleAPI === undefined) {
-					imgTitle.innerText = "Brak nazwy";
-					console.log(imgTitleAPI);
+					boxTitleDOM.innerText = "No name :(";
 				} else {
-					imgTitle.innerText = photos.current_user_collections.title;
-					console.log(imgTitleAPI);
+					boxTitleDOM.innerText = imgTitleAPI;
 				}
+				boxDOM.appendChild(boxTitleDOM);
 
 				let imgSource = photos.urls.full;
 				console.log(imgSource);
+				const imgDOM = document.createElement("img");
+				imgDOM.setAttribute("class", "unsplash-img");
+				imgDOM.setAttribute("alt", "Example Unsplash Image");
+				imgDOM.setAttribute("src", imgSource);
+				boxDOM.appendChild(imgDOM);
 
-				// if (imgClass == true) {
-				imgClass[idx].setAttribute("src", imgSource);
-				console.log(imgSource);
-				// }
+				const boxDesc = document.createElement("div");
+				boxDesc.setAttribute("class", "box-description-container");
+				boxDOM.appendChild(boxDesc);
 
-				// imgClass.setAttribute("src", imgSource);
-				// console.log(imgSource);
+				const authorInfo = document.createElement("div");
+				boxDesc.appendChild(authorInfo);
+
+				const boxDescBy = document.createElement("span");
+				boxDescBy.setAttribute("class", "by-on");
+				boxDescBy.innerText = "by";
+				authorInfo.appendChild(boxDescBy);
+
+				let userName = photos.user.name;
+				const boxDescLink = document.createElement("a");
+				boxDescLink.setAttribute("class", "description");
+				boxDescLink.innerText = userName;
+				authorInfo.appendChild(boxDescLink);
+				console.log(userName);
+
+				let portfolioUrl = photos.user.portfolio_url;
+				if (portfolioUrl !== null) {
+					boxDescLink.setAttribute("href", portfolioUrl);
+					console.log(portfolioUrl);
+				}
+
+				const dateInfo = document.createElement("div");
+				boxDesc.appendChild(dateInfo);
+
+				const boxDescOn = document.createElement("span");
+				boxDescOn.setAttribute("class", "by-on");
+				boxDescOn.innerText = "on";
+				dateInfo.appendChild(boxDescOn);
+
+				const boxDescDate = document.createElement("p");
+				boxDescDate.setAttribute("class", "description");
+				let date = new Date(photos.created_at).toLocaleDateString("en-NZ");
+				boxDescDate.innerText = date;
+				dateInfo.appendChild(boxDescDate);
+				console.log(date);
 			});
-
-			// for (let prop in data) {
-			// 	prop;
-			// 	data[prop];
-			// 	console.log(prop);
-			// 	console.log(data[prop]);
-
-			// 	let userName = data[prop].user.name;
-			// 	authorName.innerText = userName;
-			// 	let portfolioUrl = data[prop].user.portfolio_url;
-			// 	authorName.setAttribute("href", portfolioUrl);
-			// 	let date = new Date(data[prop].created_at).toLocaleDateString("en-NZ");
-			// 	createdDate.innerText = date;
-			// 	imgTitle.innerText = data[prop].current_user_collections.title;
-			// 	let imgSource = data[prop].urls.full;
-			// 	imgClass.setAttribute("src", imgSource);
-			// }
-
-			// const img = document.querySelectorAll(".unsplash-img");
-
-			// authorName.innerText = data[0].user.name;
-			// authorName.setAttribute("href", data[0].user.portfolio_url);
-			// let date = new Date(data[0].created_at).toLocaleDateString("en-NZ");
-			// createdDate.innerText = date;
-			// imgTitle.innerText = data[0].current_user_collections.title;
-			// let imgSource = data[0].urls.full;
-			// imgClass.setAttribute("src", imgSource);
 		});
 }
 
