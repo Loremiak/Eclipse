4276b09d3800e681d38e5cfcd082070f7592d972
Damian Jamróz <mrlorem@gmail.com>
2022-06-21 18:00:18 +0200

    final work

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function getAPI() {
 	fetch(
-		"https://api.unsplash.com/photos/random?count=2&client_id="
+		`https://api.unsplash.com/photos/random?count=2&client_id=${env.UNSPLASH_API_KEY}`
 	) //count=2&
 		.then((response) => {
 			return response.json();
 		})
 		.then((data) => {
-			console.log(data);
-			console.log(data[0].urls.full); // losowe img
-			console.log(data[0].user.name); // nazwa autora
-			console.log(data[0].created_at); // data i godzina
-			console.log(data[0].user.portfolio_url); // portfolio link
-			console.log(data[0].current_user_collections.title); // title
+			// console.log(data);
+			// console.log(data[0].urls.full); // losowe img
+			// console.log(data[0].user.name); // nazwa autora
+			// console.log(data[0].created_at); // data i godzina
+			// console.log(data[0].user.portfolio_url); // portfolio link
+			// console.log(data[0].current_user_collections.title); // title
 
 			const authorName = document.querySelector(".author-name");
 			const createdDate = document.querySelector(".created-date");
 			const imgTitle = document.querySelector(".img-title");
-			const imgClass = document.querySelector(".img-class");
-
-			data.forEach((photos) => {
+			const imgClass = document.querySelectorAll(".img-class");
+			console.log(imgClass);
+			data.forEach((photos, idx) => {
 				console.log(photos);
 
 				let userName = photos.user.name;
@@ -26,7 +26,6 @@ function getAPI() {
 				console.log(userName);
 
 				let portfolioUrl = photos.user.portfolio_url;
-				console.log(portfolioUrl);
 
 				if (portfolioUrl !== null) {
 					authorName.setAttribute("href", portfolioUrl);
@@ -37,14 +36,25 @@ function getAPI() {
 				createdDate.innerText = date;
 				console.log(date);
 
-				imgTitle.innerText = photos.current_user_collections.title;
-				console.log(photos.current_user_collections.title);
+				let imgTitleAPI = photos.current_user_collections.title;
+				if (imgTitleAPI === undefined) {
+					imgTitle.innerText = "Brak nazwy";
+					console.log(imgTitleAPI);
+				} else {
+					imgTitle.innerText = photos.current_user_collections.title;
+					console.log(imgTitleAPI);
+				}
 
 				let imgSource = photos.urls.full;
 				console.log(imgSource);
 
-				imgClass.setAttribute("src", imgSource);
+				// if (imgClass == true) {
+				imgClass[idx].setAttribute("src", imgSource);
 				console.log(imgSource);
+				// }
+
+				// imgClass.setAttribute("src", imgSource);
+				// console.log(imgSource);
 			});
 
 			// for (let prop in data) {
