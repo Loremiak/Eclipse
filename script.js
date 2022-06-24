0a00b76124b6126fb55a0871f6f4a510634a1a25
Damian Jamróz <mrlorem@gmail.com>
2022-06-24 16:49:37 +0200

    w

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ import TOKEN from "./config.js";
 
 function getAPI() {
 	fetch(`https://api.unsplash.com/photos/random?count=4&client_id=${TOKEN}`)
-		// process.env.UNSPLASH_API_KEY
 		.then((response) => {
 			return response.json();
 		})
